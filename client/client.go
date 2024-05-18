30d0a67d1c1728cfbe7f88d533a9ebd1eb666d54
Gert Drapers <gert.drapers@live.com>
2024-05-18 07:42:05 -0700

    remove directory v2 client

--- client/client.go
+++ client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"github.com/aserto-dev/clui"
-	v2 "github.com/aserto-dev/go-directory-cli/client/v2"
 	v3 "github.com/aserto-dev/go-directory-cli/client/v3"
 
 	"google.golang.org/grpc"
@@ -10,16 +9,11 @@ import (
 
 type Client struct {
 	conn grpc.ClientConnInterface
-	V2   *v2.Client
 	V3   *v3.Client
 	UI   *clui.UI
 }
 
 func New(conn grpc.ClientConnInterface, ui *clui.UI) (*Client, error) {
-	dsc2, err := v2.New(conn, ui)
-	if err != nil {
-		return nil, err
-	}
 	dsc3, err := v3.New(conn, ui)
 	if err != nil {
 		return nil, err
@@ -27,7 +21,6 @@ func New(conn grpc.ClientConnInterface, ui *clui.UI) (*Client, error) {
 
 	c := Client{
 		conn: conn,
-		V2:   dsc2,
 		V3:   dsc3,
 		UI:   ui,
 	}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/aserto-dev/go-directory-cli
 
-go 1.20
+go 1.21
 
 // replace github.com/aserto-dev/go-directory => ../go-directory
 
